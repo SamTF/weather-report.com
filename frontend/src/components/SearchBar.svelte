6ef6ab3a1a602e02b1a5e305e313cff11af1ff6b
SamTF <elseban88@gmail.com>
2022-02-23 22:55:17 +0000

    App fetches and displays image successfully

---
+++ frontend/src/components/SearchBar.svelte
@@ -0,0 +1,11 @@
+<!-- JS -->
+<script>
+    export let onClick
+    export let city
+</script>
+
+<!-- HTML -->
+<div class="search-bar">
+    <input type="text" name="" id="" placeholder="City" bind:value={city}>
+	<button on:click={onClick}>Get weather!!</button>
+</div>
