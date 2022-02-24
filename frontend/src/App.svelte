031b311d7f4e38f3e20654c77d5c8a3ab860f3aa
SamTF <elseban88@gmail.com>
2022-02-24 17:40:05 +0000

    Made dropdown to select between today or tomorrow

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -2,11 +2,15 @@
 <script>
 	import { onMount } from "svelte"
 	import SearchBar from './components/SearchBar.svelte'
+	import Dropdown from './components/Dropdown.svelte'
 
-	let weather_card
-	let city = ''
-	let cityLast = ''
+	let weather_card = ''		// URL for the weather card image to display
+	let city = ''				// name of the city requested by the user
+	let cityLast = ''			// the last requested city
+	let command					// weather to get current conditions or tomorrow's forecast
+	let forecastType = ''		// the text value of the LAST command requested
 
+	// Loading weather card for random location on start up
 	onMount(async () => {
 		console.log('>>> ON MOUNT')
 		const res = await fetch('http://localhost:5000/wttr/shanghai')
@@ -14,8 +18,6 @@
 		weather_card = URL.createObjectURL(card_blob)
 
 		console.log(card_blob)
-
-
 	})
 
 	// Fetching the weather card PNG from the Flask API
@@ -34,7 +36,7 @@
 		cityLast = cityName
 		
 		// fetching the data
-		const res = await fetch(`/wttr/${cityName}`)
+		const res = await fetch(`/${command.value}/${cityName}`)
 
 		// checking if the response is OK
 		if (!res.ok) {
@@ -49,6 +51,9 @@
 
 		// resetting the input
 		city = ''
+
+		// setting the appropriate title
+		forecastType = command.text
 	}
 </script>
 
@@ -61,11 +66,13 @@
 	
 	<p>This is a web UI for my <a href="https://sam.freelancepolice.org/discord_bots/david_lynch">David Lynch Weather Report</a> discord bot.</p>
 
+	<h2>{forecastType}</h2>
+	
 	<br>
 	<img src={weather_card} alt="weather card shanghai" class="weather-card">
 	<p>{weather_card}</p>
 
 	<SearchBar bind:city={city} onSubmit={fetchWeather}/>
-	
 
+	<Dropdown bind:selected={command} />
 </main>
---
+++ frontend/src/components/Dropdown.svelte
@@ -0,0 +1,17 @@
+<!-- JS -->
+<script>
+    export let selected
+
+    const options = [
+        {value: 'wttr', text: 'current conditions'},
+        {value: 'tmrw', text: 'tomorrow\'s forecast'},
+    ]
+</script>
+
+<!-- HTML -->
+<select name="today or tomorrow" id="period"
+    bind:value={selected} on:change="{() => console.log(selected)}">
+        {#each options as option}
+            <option value={option}>{option.text}</option>
+        {/each}
+</select>
