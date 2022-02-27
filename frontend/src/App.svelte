cde5d850cb0a2867c9afc1d43c455cccfd9950a3
SamTF <elseban88@gmail.com>
2022-02-27 20:24:56 +0000

    Added Dark theme toggle

--- frontend/src/App.svelte
+++ frontend/src/App.svelte
@@ -6,7 +6,7 @@
 
 	let weather_card = ''		// URL for the weather card image to display
 	let city = ''				// name of the city requested by the user
-	let cityLast = ''			// the last requested city
+	let prevCity = {wttr: '', tmrw: ''} // stores the previous cities requested for current weather and tomorrow's weather
 	let command					// weather to get current conditions or tomorrow's forecast
 	let forecastType = ''		// the text value of the LAST command requested
 
@@ -26,17 +26,20 @@
 		const cityName = city.replace(/\s+/g, ' ').trim()	// removes extras spaces from the string -> https://futurestud.io/tutorials/remove-extra-spaces-from-a-string-in-javascript-or-node-js
 
 		// check if the input is empty or is the same as the last one or contains any special character/numbers
-		if (cityName == '' || cityName == cityLast || !/^[A-Za-z\s]*$/.test(cityName)) {
-			console.error('City name cannot be repeated, empty, nor contain special characters')
-			alert('City name cannot be repeated, empty, nor contain special characters!')
-			return
+		if (cityName == '' ||
+			cityName.toLowerCase() == prevCity[command.value] ||
+			!/^[A-Za-z\s]*$/.test(cityName)) {
+				console.error('City name cannot be repeated, empty, nor contain special characters')
+				alert('City name cannot be repeated, empty, nor contain special characters!')
+				return
 		}
 
 		// remembers the last fetched city to avoid duplicate API calls
-		cityLast = cityName
+		prevCity[command.value] = city.toLowerCase()
 		
 		// fetching the data
-		const res = await fetch(`/${command.value}/${cityName}`)
+		let url = `/${command.value}/${cityName}?transparent=${darkTheme}`
+		const res = await fetch(url)
 
 		// checking if the response is OK
 		if (!res.ok) {
@@ -55,6 +58,13 @@
 		// setting the appropriate title
 		forecastType = command.text
 	}
+
+	// Toggle Light/Dark theme
+	let darkTheme = false
+	const toggleTheme = () => {
+		darkTheme = !darkTheme
+		document.documentElement.setAttribute('dark-theme', darkTheme)
+	}
 </script>
 
 <!-- HTML -->
@@ -66,7 +76,8 @@
 	
 	<p>This is a web UI for my <a href="https://sam.freelancepolice.org/discord_bots/david_lynch">David Lynch Weather Report</a> discord bot.</p>
 
-	<h2>{forecastType}</h2>
+	<!-- <h2>{forecastType}</h2> -->
+	<Dropdown bind:selected={command} />
 	
 	<br>
 	<img src={weather_card} alt="weather card shanghai" class="weather-card">
@@ -74,5 +85,5 @@
 
 	<SearchBar bind:city={city} onSubmit={fetchWeather}/>
 
-	<Dropdown bind:selected={command} />
+	<button on:click={toggleTheme}>set dark theme</button>
 </main>
